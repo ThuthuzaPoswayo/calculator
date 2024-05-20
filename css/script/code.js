c852abb75d0cd218467ec85ab38da88d117a7aba
ThuthuzaPoswayo <thuthuzaposwayo@gmail.com>
2024-05-20 12:21:30 +0200

    calculator

--- css/script/code.js
+++ css/script/code.js
@@ -1,21 +1,34 @@
 // display
-
-const display = document.getElementById("display")
-const numberButtons = document.querySelector("number")
+// let dataEntry = document.querySelector('[data-entry]')
 
 
-function clearOutput() {
+// const display = document.getElementById("display")
+// const numberButtons = document.querySelector("number")
 
-let display= document.querySelector(.'container input')
-    document.querySelector('#clearall').addEventListener('click' , () =>{
-let entry = display.value = '0'
-display.value= eval(entry)
-})
+
+/ function clearOutput() {
+
+//     const
+
+// // let display= document.querySelector(.'container input')
+// //     document.querySelector('#clearall').addEventListener('click' , () =>{
+// // let entry = display.value = '0'
+// // display.value= eval(entry)
+ })
+
     
-}
-    function clearEntry() {
-        let output = document.querySelector('#clearonce').addEventListener('click' , () => {
+ }
+     function clearEntry() {
+         let output = document.querySelector('#clearonce').addEventListener('click' , () => {
         let entry = display.value
 
-        })  value;
-    }
+//         })  value;
+ }
+
+
+ Function to clear the output
+function clearOutput() {
+    document.querySelector('input[type="text"]').value = '0';
+}
+
+
