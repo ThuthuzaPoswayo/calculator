fdc63b4ebe3ea8ecd2ee509734ef3306cb41e871
ThuthuzaPoswayo <thuthuzaposwayo@gmail.com>
2024-05-20 11:22:20 +0200

    calculator

---
+++ css/script/code.js
@@ -0,0 +1,21 @@
+// display
+
+const display = document.getElementById("display")
+const numberButtons = document.querySelector("number")
+
+
+function clearOutput() {
+
+let display= document.querySelector(.'container input')
+    document.querySelector('#clearall').addEventListener('click' , () =>{
+let entry = display.value = '0'
+display.value= eval(entry)
+})
+    
+}
+    function clearEntry() {
+        let output = document.querySelector('#clearonce').addEventListener('click' , () => {
+        let entry = display.value
+
+        })  value;
+        
